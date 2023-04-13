141773390f6bac3fbac5c391294a14d40c70b6ce
Francisco Astudillo <fjastudillo34@gmail.com>
2023-04-13 17:16:33 -0500

    Add class for connection MySQL-BD

---
+++ api-farmacia/Data/config/ConnectionBD.cs
@@ -0,0 +1,67 @@
+﻿using MySql.Data.MySqlClient;
+
+namespace Data;
+
+public class ConnectionBD
+{
+    private MySqlConnection connection;
+    private string server;
+    private string database;
+    private string uid;
+    private string password;
+    
+    public ConnectionBD()
+    {
+        Initialize();
+    }
+    
+    private void Initialize()
+    {
+        server = "localhost";
+        database = "connectcsharptomysql";
+        uid = "username";
+        password = "password";
+        string connectionString;
+        connectionString = "SERVER=" + server + ";" + "DATABASE=" + 
+                           database + ";" + "UID=" + uid + ";" + "PASSWORD=" + password + ";";
+
+        this.connection = new MySqlConnection(connectionString);
+    }
+    
+    private bool OpenConnection()
+    {
+        try
+        {
+            this.connection.Open();
+            return true;
+        }
+        catch (MySqlException ex)
+        {
+            switch (ex.Number)
+            {
+                case 0:
+                    Console.WriteLine("Cannot connect to server.  Contact administrator");
+                    break;
+
+                case 1045:
+                    Console.WriteLine("Invalid username/password, please try again");
+                    break;
+            }
+            return false;
+        }
+    }
+    
+    private bool CloseConnection()
+    {
+        try
+        {
+            connection.Close();
+            return true;
+        }
+        catch (MySqlException ex)
+        {
+            Console.WriteLine(ex.Message);
+            return false;
+        }
+    }
+}
