2f8d149dfc629ff5dbec55c49841d3367f4aa0df
Diego Alzate <diegoalzate.0207@gmail.com>
2023-04-13 23:16:57 -0500

    FAR-8, Subiendo la clase persistence y el CRUD de UserDat.cs

---
+++ api-farmacia/Data/Persistence.cs
@@ -0,0 +1,34 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data
+{
+    internal class Persistence
+    {
+        //Cadena de conexion
+        MySqlConnection _connection = new MySqlConnection(ConfigurationManager.ConnectionStrings["Conn"].ConnectionString);
+        public MySqlConnection openConnection()
+        {
+            try
+            {
+                _connection.Open();
+                return _connection;
+            }
+            catch (Exception e)
+            {
+
+                e.ToString();
+                return null;
+            }
+        }
+        public void closeConnection()
+        {
+            _connection.Close();
+        }
+    }
+}
