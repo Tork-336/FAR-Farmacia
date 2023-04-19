49209083c2746817e074b1c3f09d93a6d91bffe2
Daniel Rojas <danielrojaslopez507@gmail.com>
2023-04-18 21:04:16 -0500

    add module logic Providers

---
+++ api-farmacia/Logic/Providers/ProvidersService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Data;
+
+namespace Logic.Providers
+{
+	public class ProvidersService
+	{
+		private ProvidersService Repository;
+
+        public DataSet list()
+		{
+			return this.Repository.list();
+		}
+
+        public bool update(int id_provider, int nit_provider, string name_provider, string city_provider)
+		{
+			return this.Repository.update(id_provider, nit_provider, name_provider, city_provider);
+		}
+
+        public bool insert(int nit_provider, string name_provider, string city_provider)
+		{
+			return this.Repository.insert(nit_provider, name_provider, city_provider);
+		}
+
+        public bool delete(int id_provider)
+		{
+			return this.Repository.delete(id_provider);
+		}
+
+    }
+}
+
